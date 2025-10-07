cfafc20d4f99031d0e99543bd8def89eea890232
ben.flous300502@gmail.com <ben.flous300502@gmail.com>
2025-10-07 13:30:19 +0000

    changes for customer revenue report

---
+++ models/customer_rev/customer_stg.sql
@@ -0,0 +1,14 @@
+select
+    CustomerID,
+    FirstName,
+    LastName,
+    Email,
+    Phone,
+    Address,
+    City,
+    State,
+    ZipCode,
+    Updated_at,
+    CONCAT(FirstName, ' ', LastName) AS CustomerName
+from 
+    {{ source('landing', 'customers') }}
---
+++ models/customer_rev/orders_stg.sql
@@ -0,0 +1,21 @@
+select
+    OrderID,
+    OrderDate,
+    CustomerID,
+    EmployeeID,
+    StoreID,
+    Status as StatusCD,
+    CASE 
+        WHEN Status = '01' THEN 'In Progress'
+        WHEN Status = '02' THEN 'Completed'
+        WHEN Status = '03' THEN 'Cancelled'
+        ELSE null
+    END AS StatusDesc,
+    CASE 
+        WHEN StoreID = 1000 THEN 'Online'
+        ELSE 'In-store'
+    END AS ORDER_CHANNEL,
+    Updated_at,
+    current_timestamp as dbt_updated_at
+FROM
+    {{ source('landing', 'orders') }}
